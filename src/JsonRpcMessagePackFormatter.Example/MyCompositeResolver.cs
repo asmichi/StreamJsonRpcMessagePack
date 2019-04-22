4d670051be5ae69800e2cee7ddc81f72b1ed1f9b
asmichi <asmichi@users.noreply.github.com>
2019-04-22 20:36:10 +0900

    Add a trivial example.

---
+++ src/JsonRpcMessagePackFormatter.Example/MyCompositeResolver.cs
@@ -0,0 +1,44 @@
+// Copyright (c) @asmichi (on github). Licensed under the MIT License. See LICENCE in the project root for details.
+
+using Asmichi.StreamJsonRpcAdapters;
+using MessagePack;
+using MessagePack.Formatters;
+using MessagePack.Resolvers;
+
+namespace JsonRpcMessagePackFormatterExample
+{
+    internal sealed class MyCompositeResolver : IFormatterResolver
+    {
+        public static readonly IFormatterResolver Instance = new MyCompositeResolver();
+
+        private static readonly IFormatterResolver[] Resolvers = new[]
+        {
+            // Add JsonRpcMessagePackResolver to your resolver.
+            JsonRpcMessagePackResolver.Instance,
+
+            // Use any combination of other resolvers that meets your requirement...
+            StandardResolver.Instance,
+        };
+
+        public IMessagePackFormatter<T> GetFormatter<T>() => FormatterCache<T>.Formatter;
+
+        private static IMessagePackFormatter<T> FormatterCacheImpl<T>()
+        {
+            foreach (var item in Resolvers)
+            {
+                var f = item.GetFormatter<T>();
+                if (f != null)
+                {
+                    return f;
+                }
+            }
+
+            return null;
+        }
+
+        private static class FormatterCache<T>
+        {
+            public static readonly IMessagePackFormatter<T> Formatter = MyCompositeResolver.FormatterCacheImpl<T>();
+        }
+    }
+}
---
+++ src/JsonRpcMessagePackFormatter.Example/Program.cs
@@ -0,0 +1,49 @@
+// Copyright (c) @asmichi (on github). Licensed under the MIT License. See LICENCE in the project root for details.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Asmichi.StreamJsonRpcAdapters;
+using Nerdbank.Streams;
+using StreamJsonRpc;
+
+namespace JsonRpcMessagePackFormatterExample
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            var (streamA, streamB) = FullDuplexStream.CreatePair();
+
+            using (var sideA = CreateRpc("A", streamA))
+            using (var sideB = CreateRpc("B", streamB))
+            {
+                var ret = await sideA.InvokeAsync<int>("Mul", 4, 2);
+                Console.WriteLine(ret);
+            }
+        }
+
+        private static JsonRpc CreateRpc(string name, Stream duplexStream)
+        {
+            // Create a JsonRpcMessagePackFormatter as an IJsonRpcMessageFormatter.
+            var formatter = new JsonRpcMessagePackFormatter(MyCompositeResolver.Instance);
+
+            // Create a JsonRpc that uses the IJsonRpcMessageFormatter.
+            var handler = new LengthHeaderMessageHandler(duplexStream, duplexStream, formatter);
+            var rpc = new JsonRpc(handler)
+            {
+                TraceSource = new TraceSource(name, SourceLevels.Verbose),
+            };
+
+            rpc.AddLocalRpcTarget(new ServerObject());
+            rpc.StartListening();
+            return rpc;
+        }
+
+        private sealed class ServerObject
+        {
+            public int Mul(int a, int b) => a * b;
+        }
+    }
+}
